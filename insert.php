52b525072a0091d1aa21014c3a1b2fd3239625c5
carl <carlcodjoe09@yahoo.com>
2023-02-16 20:45:53 +0000

    New File

--- insert.php
+++ insert.php
@@ -5,13 +5,13 @@ include "connect.php";
 extract($_POST);
 
 //if (isset($_POST[''])) {
-  //  $nameSend = $_POST['completename'];
- //   $roofprofileSend = $_POST['completeroofprofile'];
- //   $roofmaterialSend = $_POST['completeroofmaterials'];
-  //  $colorSend = $_POST['completecolors'];
-  //  $weightSend = $_POST['completeweight'];
- //   $thicknessSend = $_POST['completethickness'];
-//    $remarksSend = $_POST['completeremarks'];
+// $nameSend = $_POST['completename'];
+// $roofprofileSend = $_POST['completeroofprofile'];
+// $roofmaterialSend = $_POST['completeroofmaterials'];
+// $colorSend = $_POST['completecolors'];
+// $weightSend = $_POST['completeweight'];
+// $thicknessSend = $_POST['completethickness'];
+// $remarksSend = $_POST['completeremarks'];
 
 if(isset($_POST['nameSend']) && (isset($_POST['roofprofileSend']) && (isset($_POST['roofmaterialSend']) && (isset($_POST['colorSend']) && (isset($_POST['weightSend']) && (isset($_POST['thicknessSend']) && (isset($_POST['remarksSend']))
 
@@ -20,7 +20,7 @@ if(isset($_POST['nameSend']) && (isset($_POST['roofprofileSend']) && (isset($_PO
 $sql = "INSERT INTO CRUD (customer, profile, material, thickness, color, weight, remarks)
 VALUES ('$nameSend','$roofprofileSend','$roofmaterialSend','$colorSend', '$weightSend', $thicknessSend,'$remarksSend')";
 
-    $result = mysqli_query($con, $sql) or die(mysqli_error($con));
+$result = mysqli_query($con, $sql) or die(mysqli_error($con));
 
 
 ?>
